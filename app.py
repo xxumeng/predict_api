b83db82411a3ae2472d4ce341812d632d972a1d3
xxumeng <xxu_meng@163.com>
2020-11-09 16:29:03 +0800

    端口

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route('/getJson')
+#设置编码
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/getJson',methods = ['GET'])
 def get_json():
-    return 'Hello, World!'
+    idx = request.args.get('idx')
+    return jsonify({'idx': idx, 'text1': '登录成功！'})
+@app.route('/predict',methods = ['POST'])
+def predict():
+    
+    return jsonify({'true_label': '移动业务', 'predict_label': '移动业务'})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',  # 任何ip都可以访问
+            port=7777,       # 端口
+            debug=True
+            )
--- request.py
+++ request.py
@@ -1,3 +1,21 @@
 import requests
-response = requests.get("http://127.0.0.1:5000/getJson")
-print(response.text)
+import json
+
+
+# 获得id号的数据
+def getJson(idx):
+    response = requests.get("http://127.0.0.1:7777/getJson?idx=3-1")
+    print(response.text)
+
+# 预测
+def predict(data):
+    ## headers中添加上content-type这个参数，指定为json格式
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post("http://127.0.0.1:7777/predict", headers = headers, data=json.dumps(data))
+    print(response.text)
+
+
+getJson('3-1')
+
+data = {'idx':'3-1','text1':'test'}
+predict(data)
